300ae87869640a6202c8a514fd04e30c9878e193
Fabrizio Chiarello <fabrizio.chiarello@pd.infn.it>
2017-01-16 16:52:06 +0100

    v0.0.2
    Signed-off-by: Fabrizio Chiarello <fabrizio.chiarello@pd.infn.it>

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule CaosTsdb.Mixfile do
 
   def project do
     [app: :caos_tsdb,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      test_paths: test_paths(Mix.env),
