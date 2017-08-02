40cbba5764f7505bd7bed0ebf6b7601ceb636017
Fabrizio Chiarello <fabrizio.chiarello@pd.infn.it>
2017-08-02 11:22:44 +0200

    Fix build_release.sh
    Signed-off-by: Fabrizio Chiarello <fabrizio.chiarello@pd.infn.it>

--- build_release.sh
+++ build_release.sh
@@ -58,7 +58,7 @@ docker_exec ${container_id} git checkout -f ${GIT_SHA}
 docker_exec ${container_id} ci-tools/prepare.sh
 docker_exec ${container_id} ci-tools/release-build.sh
 
-fname=${container_id}:/build/caos_tsdb-$(CI_PROJECT_DIR=. CI_COMMIT_SHA=${GIT_SHA} ci-tools/git-semver.sh).tar.gz
+fname=${container_id}:/build/releases/caos_tsdb-$(CI_PROJECT_DIR=. CI_COMMIT_SHA=${GIT_SHA} ci-tools/git-semver.sh).tar.gz
 docker cp ${fname} ${releases_dir}/
 
 docker stop ${container_id}
